f9ca894b8e5dba45775f4902ad96ba799e83e359
Kumudikatest <37790188+Kumudikatest@users.noreply.github.com>
2018-03-26 12:50:22 +0530

    test

--- Kumudikatest/lambda.js
+++ Kumudikatest/lambda.js
@@ -29,7 +29,7 @@ exports.handler = function (event, context, callback) {
 
 	ddb.put({
 		TableName: 'kumudikatest',
-		Item: { 'ID': event.ID, 'result': result }
+		Item: { 'ID': event.ID, 'Name': result }
 	}, function (err, data) {
 		if (err) {
 			console.log('fail');
