f9dff5ee8c79b346791c837ae221633bcdd1b0f8
Kumudikatest <37790188+Kumudikatest@users.noreply.github.com>
2018-04-06 10:18:48 +0530

    test

--- Kumudikatest/lambda.js
+++ Kumudikatest/lambda.js
@@ -1,4 +1,6 @@
 let AWS = require('aws-sdk');
+let SL = require('@slappforge/slappforge-sdk');
+const sqs = new SL.AWS.SQS(AWS);
 const ddb = new AWS.DynamoDB.DocumentClient();
 exports.handler = function (event, context, callback) {
 
@@ -30,17 +32,32 @@ exports.handler = function (event, context, callback) {
 
 	ddb.put({
 		TableName: 'KumuTest',
-		Item: { 'ID': id, 'Result':result}
+		Item: { 'ID': id, 'Result': result }
 	}, function (err, data) {
 		if (err) {
-			console.log('Error ',err);
+			console.log('Error ', err);
 			callback(null, err);
 		} else {
-			console.log('Data ',data);
+			console.log('Data ', data);
 			callback(null, data);
 		}
 	});
 
 	//callback(null, event);
 
+	sqs.receiveMessage({
+		QueueUrl: 'https://sqs.us-east-1.amazonaws.com/318300609668/test-queue.fifo',
+		AttributeNames: ['All'],
+		MaxNumberOfMessages: '1',
+		VisibilityTimeout: '30',
+		WaitTimeSeconds: '0',
+		MessageAttributeNames: ['']
+	}, function (receivedMessages) {
+		receivedMessages.forEach(message => {
+			// your logic to access each message through out the loop. Each message is available under variable message 
+			// within this block
+		})
+	}, function (error) {
+		// implement error handling logic here
+	});
 }
